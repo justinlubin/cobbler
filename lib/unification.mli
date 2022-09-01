ca4b5e3e425de15b67a14ea831400ea21e983059
Justin Lubin <jlubi333@gmail.com>
2022-08-31 20:32:33 -0700

    Implement simpl for unification (all that is left is match, i think)

--- lib/unification.mli
+++ lib/unification.mli
@@ -7,13 +7,24 @@
 open Core
 open Lang
 
+(** Atoms in the language supported for unification *)
+type atom =
+  | Variable of string
+  | Constant of string
+
+(** Terms in the language supported for unification *)
+type term =
+  | Atom of atom
+  | Application of term * term
+  | Abstraction of string * term
+
 (** The result of the unification algorithm *)
 type unification_result =
-  | Solved of (id * exp) list
+  | Solved of (id * term) list
   | Impossible
   | OutOfFuel
 
 (** [unify fuel e0 e0'] attempts at most [fuel] iterations of Huet's 1974
     higher-order pre-unification semi-algorithm to compute a substitution
-    [sigma] such that [sigma e0 == sigma e1]. *)
-val unify : int -> exp -> exp -> unification_result
+    [sigma] such that [sigma e0 == sigma e0']. *)
+val unify : int -> term -> term -> unification_result
--- test/test_util.ml
+++ test/test_util.ml
@@ -2,6 +2,11 @@ open Core
 open Lib
 
 let%test_unit "dedup_by 1" =
-  [%test_eq: int list]
+  [%test_result: int list]
     (Util.dedup_by ~f:(fun x y -> x mod 2 = y mod 2) [ 4; 6; 5; 2; 3; 1 ])
-    [ 4; 5 ]
+    ~expect:[ 4; 5 ]
+
+let%test_unit "find_and_remove_first 1" =
+  [%test_result: (int * int list) option]
+    (Util.find_and_remove_first ~f:(fun n -> Int.equal n 2) [ 0; 1; 2; 3; 2 ])
+    ~expect:(Some (2, [ 0; 1; 3; 2 ]))
