28deffcbc8c0175283b9a7e616d47dea8abf0260
Justin Lubin <jlubi333@gmail.com>
2022-09-26 15:24:26 -0700

    Change a unit test to result test

--- test/test_fusion.ml
+++ test/test_fusion.ml
@@ -3,7 +3,7 @@ open Lib
 open Lang
 
 let%test_unit "pull out cases 1" =
-  [%test_eq: exp]
+  [%test_result: exp]
     (Fusion.pull_out_cases
        (EMatch
           ( EMatch
@@ -12,20 +12,21 @@ let%test_unit "pull out cases 1" =
                 ; ("Just", ("x", ECtor ("Just", EApp (EVar "f", EVar "x"))))
                 ] )
           , [ ("Nothing", ("n2", EVar "zero")); ("Just", ("y", EVar "y")) ] )))
-    (EMatch
-       ( EVar "mx"
-       , [ ( "Nothing"
-           , ( "n1"
-             , EMatch
-                 ( ECtor ("Nothing", EVar "n1")
-                 , [ ("Nothing", ("n2", EVar "zero"))
-                   ; ("Just", ("y", EVar "y"))
-                   ] ) ) )
-         ; ( "Just"
-           , ( "x"
-             , EMatch
-                 ( ECtor ("Just", EApp (EVar "f", EVar "x"))
-                 , [ ("Nothing", ("n2", EVar "zero"))
-                   ; ("Just", ("y", EVar "y"))
-                   ] ) ) )
-         ] ))
+    ~expect:
+      (EMatch
+         ( EVar "mx"
+         , [ ( "Nothing"
+             , ( "n1"
+               , EMatch
+                   ( ECtor ("Nothing", EVar "n1")
+                   , [ ("Nothing", ("n2", EVar "zero"))
+                     ; ("Just", ("y", EVar "y"))
+                     ] ) ) )
+           ; ( "Just"
+             , ( "x"
+               , EMatch
+                   ( ECtor ("Just", EApp (EVar "f", EVar "x"))
+                   , [ ("Nothing", ("n2", EVar "zero"))
+                     ; ("Just", ("y", EVar "y"))
+                     ] ) ) )
+           ] ))
