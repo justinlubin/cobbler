48b44b0c35c92db597adf53a9fabd24d1cc795c7
Jeremy Ferguson <jfergpython@gmail.com>
2023-02-26 17:25:55 -0800

    added python parsing and updated main.ml to have example parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,6 @@
 open Core
 open Cbr_fp
+open Cbr_numpy
 open Lang
 
 let () =
@@ -8,17 +9,26 @@ let () =
           Some (Printf.sprintf "IllTyped(%s)" (Exp.show_multi 0 e))
       | _ -> None)
 
-let parse_file : string -> datatype_env * typ_env * env =
+let parse_file_fp : string -> datatype_env * typ_env * env =
  fun filename ->
   In_channel.with_file filename ~f:(fun file ->
-      Parse.definitions (In_channel.input_all file))
+      Cbr_fp.Parse.definitions (In_channel.input_all file))
+
+let parse_file_np : string -> Cbr_numpy.Parse.py_ast =
+  fun filename ->
+   In_channel.with_file filename ~f:(fun file ->
+      Cbr_numpy.Parse.parse_py (In_channel.input_all file))
+
 
 let file = "test/test_cbr_fp/test_data/programs/list2.lisp"
 
+let file_py = "test/test_cbr_numpy/test_data/programs/test1.py"
+
 let () =
+  print_endline "FP execution:";
   print_endline (sprintf "operating on file '%s'" file);
   printf "parsing... %!";
-  let sigma, gamma, env = parse_file file in
+  let sigma, gamma, env = parse_file_fp file in
   print_endline "parsed!";
   printf "type checking... %!";
   Type_system.well_typed (sigma, gamma, env);
@@ -33,4 +43,9 @@ let () =
   | Some e ->
       printf "solution found:\n\n%s\n\n" (Exp.show_multi 1 (Exp.clean e));
       printf "of type: %s\n\n" (Typ.show (Type_system.infer sigma gamma e)));
-  print_endline "have a nice day!"
+  print_endline "have a nice day!";
+  print_endline "Starting NumPy execution:";
+  print_endline "Parsing ... %!";
+  parse_file_np file_py |> 
+  Cbr_numpy.Parse.pprint_ast;
+  print_endline "\ndone!";
