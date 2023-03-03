144d391927b16a022c48c80e2cb7f079be1ee720
Jeremy Ferguson <jfergpython@gmail.com>
2023-03-03 01:36:33 -0800

    added name expression

--- lib/cbr_numpy/lang.ml
+++ lib/cbr_numpy/lang.ml
@@ -2,7 +2,7 @@ open Core
 
 type id = string
 type op = Add | Mul | Sub | Div | Or | And 
-type expr = BinOp of op * expr * expr | Num of int | Index of id * expr | Call of id * expr list | Str of string
+type expr = BinOp of op * expr * expr | Num of int | Index of id * expr | Call of id * expr list | Str of string | Name of id
 type stmt = Assign of expr * expr | For of expr * expr * block | Return of expr
 and block = stmt list
 type env = (id * id list, block, String.comparator_witness) Map.t
