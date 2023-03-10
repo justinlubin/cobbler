20ad932b232f0f2f8ab4d06d46c9d52a8f5f2abe
Kevin Ye <yekeviny@berkeley.edu>
2023-03-10 01:47:17 -0800

    make substitutions safe when assignment has same name as function params

--- lib/cbr_numpy/inline.ml
+++ lib/cbr_numpy/inline.ml
@@ -7,45 +7,53 @@ let rec substitute_expr : (id * expr) list -> expr -> expr =
   | Num n -> Num n
   | Str s -> Str s
   | Index (e1, e2) -> Index (substitute_expr binds e1, substitute_expr binds e2)
-  | Call (e, e_list) ->
-      Call (substitute_expr binds e, List.map (substitute_expr binds) e_list)
+  | Call (head, args) ->
+      Call (substitute_expr binds head, List.map (substitute_expr binds) args)
   | Name id when List.mem_assoc id binds -> List.assoc id binds
   | Name id -> Name id
-
 
 and substitute_lhs : (id * expr) list -> lhs -> lhs =
  fun binds lhs ->
   match lhs with
-  | Index (lhs, e) -> Index (substitute_lhs binds lhs, substitute_expr binds e)
+  | Index (new_lhs, e) ->
+      Index (substitute_lhs binds new_lhs, substitute_expr binds e)
   | Name id -> Name id
 
-and substitute_stmt : (id * expr) list -> stmt -> stmt =
+and substitute_stmt : (id * expr) list -> stmt -> (id * expr) list * stmt =
  fun binds stmt ->
   match stmt with
-  | Assign (lhs, e) -> Assign (substitute_lhs binds lhs, substitute_expr binds e)
+  | Assign (lhs, e) ->
+      (match lhs with
+      | Name id ->
+          (List.remove_assoc id binds, Assign (Name id, substitute_expr binds e))
+      | lhs ->
+          (binds, Assign (substitute_lhs binds lhs, substitute_expr binds e)))
   | For (id, e, block) ->
-      For (id, substitute_expr binds e, substitute_block binds block)
-  | Return e -> Return (substitute_expr binds e)
+      ( List.remove_assoc id binds
+      , For (id, substitute_expr binds e, substitute_block binds block) )
+  | Return e -> (binds, Return (substitute_expr binds e))
 
 and substitute_block : (id * expr) list -> block -> block =
- fun binds block -> List.map (substitute_stmt binds) block
+ fun binds body ->
+  let _, block = List.fold_left_map substitute_stmt binds body in
+  block
 
 let substitute : expr list -> id list -> block -> block * expr =
  fun args params body ->
   let block = substitute_block (List.combine params args) body in
   match List.rev block with
-  | Return e :: rev -> (List.rev rev, e)
+  | Return e :: rev_body -> (List.rev rev_body, e)
   | _ -> failwith "return statement not found in function"
 
 let rec inline_expr : env -> expr -> block * expr =
  fun env e ->
   match e with
-  | Call (e, args) ->
-      (match e with
+  | Call (head, args) ->
+      (match head with
       | Name fn ->
           (match find env fn with
           | Some (params, body) -> substitute args params body
-          | None -> ([], Call (e, args)))
+          | None -> ([], Call (head, args)))
       | _ -> ([], e))
   | _ -> ([], e)
 
--- test/test_cbr_numpy/test_inline.ml
+++ test/test_cbr_numpy/test_inline.ml
@@ -12,7 +12,13 @@ let sum_defn =
     ; Return (Name "count")
     ] )
 
-let env : env = Map.of_alist_exn (module String) [ ("sum", sum_defn) ]
+let f_defn = ([ "x" ], [ Assign (Name "x", Str "cat"); Return (Name "x") ])
+let id_defn = ([ "x" ], [ Return (Name "x") ])
+
+let env : env =
+  Map.of_alist_exn
+    (module String)
+    [ ("sum", sum_defn); ("f", f_defn); ("id", id_defn) ]
 
 let p1 =
   ( env
@@ -24,15 +30,35 @@ let p2 =
   , [ Return (Call (Name "sum", [ Call (Name "add", [ Name "x"; Name "y" ]) ]))
     ] )
 
-let inlined1 =
+let p3 = (env, [ Assign (Name "x", Num 5); Return (Call (Name "f", [ Num 0 ])) ])
+
+let p4 =
+  (env, [ Assign (Name "x", Num 5); Return (Call (Name "id", [ Num 0 ])) ])
+
+let inline_of_p1 =
   ( env
   , [ Assign (Name "count", Num 0)
     ; For ("xi", Call (Name "add", [ Name "x"; Name "y" ]), sum_body)
     ; Return (Name "count")
     ] )
 
+let inline_of_p3 =
+  ( env
+  , [ Assign (Name "x", Num 5)
+    ; Assign (Name "x", Str "cat")
+    ; Return (Name "x")
+    ] )
+
+let inline_of_p4 = (env, [ Assign (Name "x", Num 5); Return (Num 0) ])
+
 let%test_unit "inline 1" =
-  [%test_result: program] (inline_program p1) ~expect:inlined1
+  [%test_result: program] (inline_program p1) ~expect:inline_of_p1
 
 let%test_unit "empty env inline" =
   [%test_result: program] (inline_program p2) ~expect:p2
+
+let%test_unit "inline with name conflict" =
+  [%test_result: program] (inline_program p3) ~expect:inline_of_p3
+
+let%test_unit "inline 4" =
+  [%test_result: program] (inline_program p4) ~expect:inline_of_p4
