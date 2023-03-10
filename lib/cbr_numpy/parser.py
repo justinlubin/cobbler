a98547135aaac76e51e307b38f02c1f15d5d7cac
Jeremy Ferguson <jfergpython@gmail.com>
2023-03-10 14:23:18 -0800

    Changed parser to make it work with python >= 3.7

--- lib/cbr_numpy/parser.py
+++ lib/cbr_numpy/parser.py
@@ -20,7 +20,7 @@ class UnsupportedFeatureException(Exception):
 class IRParser(ast.NodeVisitor):
 
     def getClassName(self, node):
-        pattern = "<class 'ast\.([\w]+)'>"
+        pattern = "<class '_?ast\\.([\\w]+)'>"
         match = re.search(pattern, str(node.__class__))
         if match:
             name = match.group(1)
@@ -40,14 +40,15 @@ class IRParser(ast.NodeVisitor):
     def visit_Assign(self, node):
         if len(node.targets) > 1:
             raise UnsupportedFeatureException("Multiple assignments")
-        return SList([SAtom("Assign"), self.visit(node.targets[0]), self.visit(node.value)])
+        return SList([SAtom("Assign"), self.visit(
+            node.targets[0]), self.visit(node.value)])
 
     def visit_Attribute(self, node):
-        match node.value:
-            case ast.Name(id=name):
-                return SAtom(f"{name}_{node.attr}")
-            case other:
-                raise UnspportedFeatureException("Attribute")
+        className = self.getClassName(node.value)
+        if className == "Name":
+            return SAtom(f"{node.value.id}_{node.attr}")
+        else:
+            raise UnsupportedFeatureException("Unsupported attribute feature")
 
     def visit_Name(self, node):
         return SAtom(node.id)
@@ -62,16 +63,22 @@ class IRParser(ast.NodeVisitor):
         return SAtom("-")
 
     def visit_Subscript(self, node):
-        return SList([SAtom("Index"), self.visit(node.value), self.visit(node.slice)])
+        return SList([SAtom("Index"), self.visit(
+            node.value), self.visit(node.slice)])
 
     def visit_AugAssign(self, node):
         target = self.visit(node.target)
-        return SList([SAtom("Assign"), target, SList([SAtom("Call"), self.visit(node.op), target, self.visit(node.value)])])
+        return SList([SAtom("Assign"), target, SList(
+            [SAtom("Call"), self.visit(node.op), target, self.visit(node.value)])])
+
+    def visit_Index(self, node):
+        return self.visit(node.value)
 
     def visit_For(self, node):
         if node.orelse:
             raise UnsupportedFeatureException("For-else statement")
-        return SList([SAtom("For"), self.visit(node.target), self.visit(node.iter), SList([self.visit(stmt) for stmt in node.body])])
+        return SList([SAtom("For"), self.visit(node.target), self.visit(
+            node.iter), SList([self.visit(stmt) for stmt in node.body])])
 
     def visit_Module(self, node):
         block = []
@@ -79,24 +86,28 @@ class IRParser(ast.NodeVisitor):
         for stmt in node.body:
             child = self.visit(stmt)
             if child:
-                match self.getClassName(stmt):
-                    case "FunctionDef":
-                        env.append(child)
-                    case other:
-                        block.append(child)
+                if self.getClassName(stmt) == "FunctionDef":
+                    env.append(child)
+                else:
+                    block.append(child)
         return SList([SList(env), SList(block)])
 
     def visit_Constant(self, node):
         return SList([SAtom("Num"), SAtom(str(node.n))])
 
+    def visit_Num(self, node):
+        return SList([SAtom("Num"), SAtom(str(node.n))])
+
     def visit_Call(self, node):
-        return SList([SAtom("Call"), self.visit(node.func)] + [self.visit(arg) for arg in node.args])
+        return SList([SAtom("Call"), self.visit(node.func)] +
+                     [self.visit(arg) for arg in node.args])
 
     def visit_Operator(self, node):
         return SAtom(self.getClassName(node))
 
     def visit_BinOp(self, node):
-        return SList([SAtom("Call"), self.visit(node.op), self.visit(node.left), self.visit(node.right)])
+        return SList([SAtom("Call"), self.visit(node.op),
+                     self.visit(node.left), self.visit(node.right)])
 
     def visit_Arg(self, node):
         print(node.arg)
