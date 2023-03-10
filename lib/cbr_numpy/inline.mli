0832d46de2e3751bf8865edb9cfbb6cca776921e
Kevin Ye <yekeviny@berkeley.edu>
2023-03-10 02:06:41 -0800

    added doc text to inline.mli and partial_eval.mli

--- lib/cbr_numpy/inline.mli
+++ lib/cbr_numpy/inline.mli
@@ -1,3 +1,11 @@
+(** Inlining
+    
+    This module provides a function that inlines a {!val:Lang.program},
+    replacing functions found in {!val:Lang.expr}'s at the highest level *)
+
 open Lang
 
+(** [inline_program (env, block)] inlines [block] by replacing 
+    {!val:Lang.expr}'s at the highest level that are calls to
+    functions in [env] *)
 val inline_program : program -> program
--- lib/cbr_numpy/partial_eval.mli
+++ lib/cbr_numpy/partial_eval.mli
@@ -1,3 +1,15 @@
+(** Partial evaluation
+    
+    This module provides a function that performs partial evaluation
+    on a {!val:Lang.program} *)
+
 open Lang
 
+(** [partial_eval_program p] partially evaluates p by making 
+    the following transformations:
+
+    - len(mul(x,y)) -> len(x)
+    - mul(x,y)[i] -> x[i] * y[i]
+    
+    *)
 val partial_eval_program : program -> program
