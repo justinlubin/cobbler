5917267f811e8dd6f0712bdb2abdb7cac922eca4
Kevin Ye <yekeviny@berkeley.edu>
2023-04-04 22:49:05 -0700

    added documentation for Cbr_numpy.Env and Cbr_numpy.Np_synthesis, updated doc for unification

--- lib/cbr_numpy/env.mli
+++ lib/cbr_numpy/env.mli
@@ -1,3 +1,12 @@
+(** Env
+
+    This module contains an env for numpy functions.
+*)
+
 open Lang
 
+(** [np_env] contains definitions of the following numpy functions:
+    
+    - sum(x)
+*)
 val np_env : env
--- lib/cbr_numpy/np_synthesis.mli
+++ lib/cbr_numpy/np_synthesis.mli
@@ -1,3 +1,15 @@
+(** Np_synthesis
+
+    This module contains the [solve] function which uses top down enumeration
+    to find numpy code that is equivalent to a given python program input.
+*)
+
 open Lang
 
+(** [solve depth target] uses top down enumeration with search space of level [depth]
+    to enumerate candidate program sketches, which are then canonicalized with partial eval
+    and inlining and unified against [target] to determine a match. If any sketches match,
+    the resulting substitution is made in the original sketch and outputed. If no solution
+    is found, [None] is returned.
+*)
 val solve : int -> program -> program option
