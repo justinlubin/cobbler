9761cafec311b700f00a6ca2889fb16480afe473
Kevin Ye <yekeviny@berkeley.edu>
2023-04-21 16:51:20 -0700

    uncommented out unify tests, added some tests for unify testing duplicate hole names

--- test/test_cbr_numpy/test_unify.ml
+++ test/test_cbr_numpy/test_unify.ml
@@ -5,22 +5,36 @@ open Unification
 open Lang
 
 let reference1 : program =
-  ( String.Map.of_alist_exn []
-  , [ Assign (PName "x", Call (Name "+", [ Num 1; Num 2 ])) ] )
+  (String.Map.empty, [ Assign (PName "x", Call (Name "+", [ Num 1; Num 2 ])) ])
 
 let reference2 : program =
-  ( String.Map.of_alist_exn []
+  ( String.Map.empty
   , [ Assign
         ( PName "x"
         , Call (Name "+", [ Call (Name "*", [ Num 2; Num 3 ]); Num 2 ]) )
     ] )
 
+let reference4 : program =
+  (String.Map.empty, [ Assign (PName "x", Call (Name "+", [ Num 2; Num 2 ])) ])
+
+let reference4' : program =
+  ( String.Map.empty
+  , [ Assign
+        ( PName "x"
+        , Call
+            ( Name "+"
+            , [ Index (Name "a", Name "i"); Index (Name "a", Name "i") ] ) )
+    ] )
+
+let reference4'' : program =
+  (String.Map.empty, [ Assign (PName "x", Call (Name "+", [ Num 1; Num 2 ])) ])
+
 let candidate1 : program =
-  ( String.Map.of_alist_exn []
+  ( String.Map.empty
   , [ Assign (PName "x", Call (Name "+", [ Hole (Number, "1"); Num 2 ])) ] )
 
 let candidate2 : program =
-  ( String.Map.of_alist_exn []
+  ( String.Map.empty
   , [ Assign
         ( PName "x"
         , Call
@@ -31,15 +45,15 @@ let candidate2 : program =
     ] )
 
 let candidate3 : program =
-  ( String.Map.of_alist_exn []
+  ( String.Map.empty
   , [ Assign
         (PName "x", Call (Name "+", [ Hole (Number, "1"); Hole (Number, "2") ]))
     ] )
 
 let candidate4 : program =
-  ( String.Map.of_alist_exn []
+  ( String.Map.empty
   , [ Assign
-        (PName "x", Call (Name "+", [ Hole (Number, "1"); Hole (Number, "1") ]))
+        (PName "x", Call (Name "+", [ Hole (Number, "x"); Hole (Number, "x") ]))
     ] )
 
 let unify_raises_error : program -> program -> bool =
@@ -48,11 +62,8 @@ let unify_raises_error : program -> program -> bool =
   | exception s -> true
   | _ -> false
 
-(*
-
 let%test_unit "simple hole substitution" =
   [%test_result: substitutions option]
-    (unify ~target:reference1 ~pattern:candidate1)
     (unify ~target:reference1 ~pattern:candidate1)
     ~expect:(Some (String.Map.of_alist_exn [ ("1", Num 1) ]))
 
@@ -66,8 +77,6 @@ let%test_unit "2 hole substitutions" =
     (unify ~target:reference1 ~pattern:candidate3)
     ~expect:(Some (String.Map.of_alist_exn [ ("1", Num 1); ("2", Num 2) ]))
 
-(* let%test "duplicate hole value" = unify_raises_error reference1 candidate4 *)
-
 let%test_unit "more complex hole substitution" =
   [%test_result: substitutions option]
     (unify ~target:reference2 ~pattern:candidate1)
@@ -75,4 +84,18 @@ let%test_unit "more complex hole substitution" =
       (Some
          (String.Map.of_alist_exn [ ("1", Call (Name "*", [ Num 2; Num 3 ])) ]))
 
-  \*)
+let%test_unit "duplicate hole substitution" =
+  [%test_result: substitutions option]
+    (unify ~target:reference4 ~pattern:candidate4)
+    ~expect:(Some (String.Map.of_alist_exn [ ("x", Num 2) ]))
+
+let%test_unit "duplicate hole substitution with complex expr" =
+  [%test_result: substitutions option]
+    (unify ~target:reference4' ~pattern:candidate4)
+    ~expect:
+      (Some (String.Map.of_alist_exn [ ("x", Index (Name "a", Name "i")) ]))
+
+let%test_unit "duplicate hole substitution fail for unmatching exprs" =
+  [%test_result: substitutions option]
+    (unify ~target:reference4'' ~pattern:candidate4)
+    ~expect:None
