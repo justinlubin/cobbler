263cfd567a3622b7cd285afde209094446c6840a
Jacob Yim <jayimgs@gmail.com>
2023-04-28 20:14:28 -0700

    rerun autoformatter

--- benchmark/main.ml
+++ benchmark/main.ml
@@ -13,11 +13,7 @@ let () =
     | _ -> failwith "Invalid target type"
   in
   let input = read_line () in
-  let target =
-    input
-    |> Sexp.of_string
-    |> Parse.program_of_sexp
-  in
+  let target = input |> Sexp.of_string |> Parse.program_of_sexp in
   match Cbr_numpy.Np_synthesis.solve 5 ~debug:false target_type target true with
   | None -> print_endline "no solution found"
   | Some e -> print_endline (py_str_of_program e)
