1bdd6fc1ab3884af83e5f20fb80297590b476199
Justin Lubin <jlubi333@gmail.com>
2023-05-25 11:23:57 -0700

    Add some test Elm programs + script to generate json tests

---
+++ test/test_cbr_fp/test_data/elm-programs/src/Sugar.elm
@@ -0,0 +1,39 @@
+module Sugar exposing (..)
+
+
+map : (a -> b) -> List a -> List b
+map f xs =
+    case xs of
+        [] ->
+            []
+
+        hd :: tl ->
+            f hd :: map f tl
+
+
+filter : (a -> Bool) -> List a -> List a
+filter pred xs =
+    case xs of
+        [] ->
+            []
+
+        hd :: tl ->
+            if pred hd then
+                filter pred tl
+
+            else
+                hd :: filter pred tl
+
+
+target : (a -> Bool) -> (a -> b) -> List a -> List b
+target pred f xs =
+    case xs of
+        [] ->
+            []
+
+        hd :: tl ->
+            if pred hd then
+                target pred f tl
+
+            else
+                f hd :: target pred f tl
