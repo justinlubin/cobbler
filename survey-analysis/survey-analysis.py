0cd175e6a1230583e5deec1d1c4dcfe83dc2c952
Justin Lubin <jlubi333@gmail.com>
2023-10-20 15:07:07 -0700

    Work on survey analysis

--- survey-analysis/survey-analysis.py
+++ survey-analysis/survey-analysis.py
@@ -2,9 +2,16 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import warnings
 
-warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
+import util
+
+warnings.simplefilter(
+    action="ignore",
+    category=pd.errors.PerformanceWarning,
+)
 
 # %% Load data files
 
@@ -17,7 +24,7 @@ raw_data = pd.read_csv(
 
 # %% Clean data
 
-data = (
+cleaned_raw_data = (
     raw_data.drop(
         columns=raw_data.columns[raw_data.columns.str.startswith("FL_")],
     )
@@ -25,16 +32,17 @@ data = (
     .fillna("")
 )
 
-metadata = pd.DataFrame(index=data.index)
-metadata["PL1"] = data.iloc[:, 10]
-metadata["PL2"] = data.iloc[:, 11]
-metadata["Exp"] = data.iloc[:, 12]
+metadata = pd.DataFrame(index=cleaned_raw_data.index)
+metadata["PL1"] = cleaned_raw_data.iloc[:, 10]
+metadata["PL2"] = cleaned_raw_data.iloc[:, 11]
+metadata["Exp"] = cleaned_raw_data.iloc[:, 12]
 
-readdata = pd.DataFrame(index=data.index)
-preferdata = pd.DataFrame(index=data.index)
+readdata = pd.DataFrame(index=cleaned_raw_data.index)
+preferdata = pd.DataFrame(index=cleaned_raw_data.index)
 
-
-comparison_columns = data.columns[data.columns.str.startswith("QID")]
+comparison_columns = cleaned_raw_data.columns[
+    cleaned_raw_data.columns.str.startswith("QID")
+]
 
 questions = {}
 
@@ -42,32 +50,32 @@ a = "Option (A)"
 b = "Option (B)"
 
 
-def correct(row):
-    row = row.values
-    if row[0] == a:
-        if row[1] == a:
+def clean_duplicates(row):
+    r = row.values
+    if r[0] == a:
+        if r[1] == a:
             return ""
-        elif row[1] == b:
+        elif r[1] == b:
             return "I"
-        elif row[1] == "":
+        elif r[1] == "":
             return "I"
         else:
             assert False
-    elif row[0] == b:
-        if row[1] == a:
+    elif r[0] == b:
+        if r[1] == a:
             return "O"
-        elif row[1] == b:
+        elif r[1] == b:
             return ""
-        elif row[1] == "":
+        elif r[1] == "":
             return "O"
         else:
             assert False
-    elif row[0] == "":
-        if row[1] == a:
+    elif r[0] == "":
+        if r[1] == a:
             return "O"
-        elif row[1] == b:
+        elif r[1] == b:
             return "I"
-        elif row[1] == "":
+        elif r[1] == "":
             return ""
         else:
             assert False
@@ -92,60 +100,145 @@ for q in range(0, len(comparison_columns), 6):
     questions[qname] = qval
 
     preferdata[qname] = (
-        data[[comparison_columns[q], comparison_columns[q + 3]]]
-    ).apply(correct, axis=1)
+        cleaned_raw_data[[comparison_columns[q], comparison_columns[q + 3]]]
+    ).apply(clean_duplicates, axis=1)
 
     readdata[qname] = (
-        data[[comparison_columns[q + 1], comparison_columns[q + 4]]]
-    ).apply(correct, axis=1)
+        cleaned_raw_data[[comparison_columns[q + 1], comparison_columns[q + 4]]]
+    ).apply(clean_duplicates, axis=1)
+
 
 # %% Make summary
 
-readsummary = pd.DataFrame(
-    index=["M-I", "R-I", "L-I", "M-O", "R-O", "L-O"],
-)
 
-readsummary["Type"] = readsummary.index.str[0]
-readsummary["IO"] = readsummary.index.str[-1]
-readsummary["Count"] = 0
-readsummary["Total"] = 0
+def type_of_char(c):
+    if c == "M":
+        return "Maybe"
+    elif c == "R":
+        return "Result"
+    elif c == "L":
+        return "List"
+    else:
+        assert False
 
-prefersummary = pd.DataFrame(
-    index=["M-I", "R-I", "L-I", "M-O", "R-O", "L-O"],
-)
 
-prefersummary["Type"] = prefersummary.index.str[0]
-prefersummary["IO"] = prefersummary.index.str[-1]
-prefersummary["Count"] = 0
-prefersummary["Total"] = 0
+def cs_of_char(c):
+    if c == "I":
+        return "Direct (input)"
+    elif c == "O":
+        return "Combinator (output)"
+    else:
+        assert False
 
-for c in readdata.columns:
-    qt = c[-1:]
-    isum = (readdata[c] == "I").sum()
-    osum = (readdata[c] == "O").sum()
-    readsummary.loc[f"{qt}-I", "Count"] += isum
-    readsummary.loc[f"{qt}-I", "Total"] += isum + osum
-    readsummary.loc[f"{qt}-O", "Count"] += osum
-    readsummary.loc[f"{qt}-O", "Total"] += isum + osum
 
-for c in preferdata.columns:
-    qt = c[-1:]
-    isum = (preferdata[c] == "I").sum()
-    osum = (preferdata[c] == "O").sum()
-    prefersummary.loc[f"{qt}-I", "Count"] += isum
-    prefersummary.loc[f"{qt}-I", "Total"] += isum + osum
-    prefersummary.loc[f"{qt}-O", "Count"] += osum
-    prefersummary.loc[f"{qt}-O", "Total"] += isum + osum
+def summarize(data):
+    summary = pd.DataFrame(
+        index=[
+            "M-I",
+            "M-O",
+            "R-I",
+            "R-O",
+            "L-I",
+            "L-O",
+        ],
+    )
 
-readsummary["Percent"] = readsummary["Count"] / readsummary["Total"]
-prefersummary["Percent"] = prefersummary["Count"] / prefersummary["Total"]
+    summary["Type"] = summary.index.str[0].map(type_of_char)
+    summary["Code Style"] = summary.index.str[-1].map(cs_of_char)
+    summary["Count"] = 0
+    summary["Total"] = 0
+
+    for c in data.columns:
+        qt = c[-1:]
+        isum = (data[c] == "I").sum()
+        osum = (data[c] == "O").sum()
+        summary.loc[f"{qt}-I", "Count"] += isum
+        summary.loc[f"{qt}-I", "Total"] += isum + osum
+        summary.loc[f"{qt}-O", "Count"] += osum
+        summary.loc[f"{qt}-O", "Total"] += isum + osum
+
+    summary["Percent"] = summary["Count"] / summary["Total"]
+
+    return summary
+
+
+readsummary = summarize(readdata)
+prefersummary = summarize(preferdata)
+
+# %% Make sums
+
+
+def make_sums(data):
+    sums = pd.DataFrame(index=data.index)
+    sums["I Sum"] = 0
+    sums["O Sum"] = 0
+
+    for idx, row in data.iterrows():
+        sums.loc[idx, "I Sum"] += (row == "I").sum()
+        sums.loc[idx, "O Sum"] += (row == "O").sum()
+
+    sums["Percent-O"] = sums["O Sum"] / (sums["I Sum"] + sums["O Sum"])
+    return sums
+
+
+readsums = make_sums(readdata)
+prefersums = make_sums(preferdata)
+
 
 # %% Make bar plots
 
-g = sns.catplot(
-    data=readsummary,
-    x="Type",
-    y="Percent",
-    hue="IO",
-    kind="bar",
-)
+
+def simple_bars(summary, adjective):
+    fig, ax = plt.subplots(1, 1, figsize=(3, 4))
+
+    util.bar_plot(
+        ax,
+        y=summary["Percent"],
+        group_labels=summary["Type"],
+        legend_labels=summary["Code Style"],
+        legend_label_colors={
+            "Direct (input)": "#BC89C5",
+            "Combinator (output)": "#73D8F8",
+        },
+    )
+
+    ax.set_ylim(0, 1)
+    ax.set_yticks(np.arange(0, 1.1, 0.2))
+    ax.yaxis.set_major_formatter(mtick.FuncFormatter("{:.0%}".format))
+
+    # fig.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
+    # fig.legend(loc="center left", bbox_to_anchor=(1, 0))
+    fig.tight_layout()
+    # fig.suptitle(f"Which style is {adjective}?")
+
+
+simple_bars(readsummary, r"more $\bf{readable}$")
+simple_bars(prefersummary, r"$\bf{preferred}$")
+
+# %% Make experience scatter plots
+
+
+def exp_scatter(exp_data):
+    fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+    ax.scatter(exp_data["Exp"], exp_data["Percent-O"])
+
+    ax.set_xlim(0, 100)
+    ax.set_ylim(0, 1)
+    ax.set_yticks(np.arange(0, 1.1, 0.2))
+    ax.yaxis.set_major_formatter(mtick.FuncFormatter("{:.0%}".format))
+
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+    ax.set_ylabel("Combinator style preferred")
+    ax.set_xlabel("Years experience with STFP")
+
+    fig.tight_layout()
+
+
+read_exp_data = metadata.join(readsums)[["Exp", "Percent-O"]]
+prefer_exp_data = metadata.join(prefersums)[["Exp", "Percent-O"]]
+
+read_exp_data["Exp"] = np.random.random(2) * 100
+
+exp_scatter(read_exp_data)
---
+++ survey-analysis/util.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bar_plot(
+    ax,
+    y=None,
+    group_labels=None,
+    legend_labels=None,
+    legend_label_colors=None,
+    width=0.8,
+    na_zero_y_offset=0,
+    group_spacing=1,
+):
+    """
+    Plots a highly-configurable bar plot.
+
+    Arguments:
+        ax: The matplotlib axis to make the bar plot on
+
+        y: The heights of the bars in the bar plot (one per bar)
+
+        group_labels: a categorical label to assign each bar (one per bar);
+            displayed on the x-axis
+
+        legend_labels: a categorical label to assign each bar (one per bar);
+            display in the legend
+
+        legend_label_colors: the colors to assign each bar, based on its legend
+            label; stored as a dictionary from legend labels to colors
+
+        width: the width of the bar between 0 (exclusive) and 1 (inclusive)
+
+        na_zero_y_offset: the y-position to plot the NA/0 textual indicator
+
+        group_spacing: the amount of space (measured in bar widths) to separate
+            each group
+
+    """
+
+    offset = 0
+    xticks = []
+    xticklabels = []
+    group_start = None
+    prev_group_label = None
+
+    used_legend_labels = set()
+
+    for yy, group_label, legend_label in zip(y, group_labels, legend_labels):
+        if group_label != prev_group_label:
+            if prev_group_label is not None:
+                xticks.append((group_start + (offset - 1)) / 2)
+                xticklabels.append(prev_group_label)
+                offset += group_spacing
+
+            group_start = offset
+
+        color = legend_label_colors[legend_label]
+        if legend_label in used_legend_labels:
+            legend_label = None
+        else:
+            used_legend_labels.add(legend_label)
+
+        ax.bar(
+            offset,
+            yy,
+            width=width,
+            color=color,
+            label=legend_label,
+        )
+
+        if yy == 0:
+            ax.text(
+                offset,
+                na_zero_y_offset,
+                "0",
+                ha="center",
+                va="bottom",
+                color=color,
+                size=7,
+            )
+
+        if np.isnan(yy):
+            ax.text(
+                offset,
+                na_zero_y_offset,
+                "NA",
+                ha="center",
+                va="bottom",
+                color=color,
+                size=7,
+            )
+
+        offset += 1
+        prev_group_label = group_label
+
+    xticks.append((group_start + (offset - 1)) / 2)
+    xticklabels.append(prev_group_label)
+
+    ax.set_xlim(-1, offset)
+    ax.set_xticks(xticks, labels=xticklabels)
+
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
