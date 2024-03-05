df3e5d6b44b52b47aaf3fc2c61494727626c3405
Justin Lubin <jlubi333@gmail.com>
2024-03-05 13:51:08 -0800

    More evaluation cleaning

--- evaluation/run_everything.sh
+++ evaluation/run_everything.sh
@@ -2,9 +2,9 @@
 
 SCRIPTS="evaluation/scripts"
 
-# bash "$SCRIPTS/download_extract_real_world.sh"
-# bash "$SCRIPTS/run_synthesis_eval.sh"
-# python "$SCRIPTS/run_numpy_perf_eval.py"
+bash "$SCRIPTS/download_extract_real_world.sh"
+bash "$SCRIPTS/run_synthesis_eval.sh"
+python "$SCRIPTS/run_numpy_perf_eval.py"
 python "$SCRIPTS/analyze_synthesis_eval.py"
 python "$SCRIPTS/analyze_numpy_perf_eval.py"
 python "$SCRIPTS/analyze_survey_results.py"
