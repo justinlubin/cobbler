0b81a1a24005975108ab29e9cc230438e56448ca
Justin Lubin <jlubi333@gmail.com>
2024-03-16 21:10:45 -0700

    Touch up ablation analysis script

--- evaluation/scripts/analyze_ablation.py
+++ evaluation/scripts/analyze_ablation.py
@@ -15,12 +15,18 @@ OUTPUT_DIR = "evaluation/output/analyses"
 
 def summarize(filename, drop):
     df = pd.read_csv(f"{INPUT_DIR}/{filename}", sep="\t")
-    return len(
-        df[df["status"] == "Success"]
-        .reset_index()
-        .drop(
-            {r - 2 for r in drop},
-            errors="ignore",
+    return dict(
+        zip(
+            *np.unique(
+                df[df["status"] == "Success"]
+                .reset_index()
+                .drop(
+                    {r - 2 for r in drop},
+                    errors="ignore",
+                )["synthed ast size"]
+                .astype(int),
+                return_counts=True,
+            )
         )
     )
 
@@ -44,20 +50,50 @@ python_ablation = summarize(
 
 # %% Plot data
 
+NUMBERS = "⓪①②③④⑤"
 
-def plot(data, data_ablation, title):
+
+def plot(data, data_ablation, title, *, sizes):
     fig, ax = plt.subplots(1, 1, figsize=(2, 3))
-    b = ax.bar(
-        [0, 1],
-        [data, data_ablation],
-        width=0.7,
-        color="gray",
-        edgecolor="black",
-    )
-    ax.bar_label(b)
+
+    data_total = sum(data.values())
+    data_ablation_total = sum(data_ablation.values())
+
+    if sizes:
+        max_size = max(max(data.keys()), max(data_ablation.keys()))
+        bottom = [0, 0]
+        for size in range(1, max_size + 1):
+            size_label = NUMBERS[size]
+            b = ax.bar(
+                [0, 1],
+                [data[size], data_ablation[size]],
+                bottom=bottom,
+                width=0.7,
+                color="gray",
+                edgecolor="black",
+            )
+            ax.bar_label(
+                b,
+                labels=[size_label, size_label],
+                label_type="center",
+                fontsize=14,
+            )
+            bottom[0] += data[size]
+            bottom[1] += data_ablation[size]
+        ax.bar_label(b)
+    else:
+        b = ax.bar(
+            [0, 1],
+            [data_total, data_ablation_total],
+            width=0.7,
+            color="gray",
+            edgecolor="black",
+        )
+        ax.bar_label(b)
+
     ax.set_xticks([0, 1], labels=["Full", "Ablated"])
 
-    max_count = max(data, data_ablation)
+    max_count = max(data_total, data_ablation_total)
     ystep = 20 if max_count < 300 else 500
     ax.set_ylim(0, max_count * 1.1)
     ax.set_yticks(np.arange(0, max_count, ystep))
@@ -72,5 +108,5 @@ def plot(data, data_ablation, title):
     fig.savefig(f"{OUTPUT_DIR}/{title}-ablation.pdf")
 
 
-plot(elm, elm_ablation, "Elm")
-plot(python, python_ablation, "Python")
+plot(elm, elm_ablation, "Elm", sizes=False)
+plot(python, python_ablation, "Python", sizes=False)
